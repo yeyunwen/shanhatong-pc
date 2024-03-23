87112da3da9ac97d9721c01f67f9b53753dd616a
yeyunwen <1571374338@qq.com>
2024-03-23 21:17:31 +0800

    chore: 添加prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: ["@nuxt/eslint-config", "prettier"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
 };
