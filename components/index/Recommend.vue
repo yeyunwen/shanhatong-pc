031191183d25eb4fba3a52f953f0b888d0718869
yeyunwen <1571374338@qq.com>
2024-03-23 21:16:28 +0800

    feat: 首页 画页面

---
+++ components/index/Recommend.vue
@@ -0,0 +1,104 @@
+<script lang="ts" setup>
+const data = [
+  {
+    id: 1,
+    title: "滚滚长江东逝水",
+    desc: "滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水",
+    img: "https://picsum.photos/122/122",
+  },
+  {
+    id: 2,
+    title: "滚滚长江东逝水",
+    desc: "滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水",
+    img: "https://picsum.photos/122/122",
+  },
+  {
+    id: 3,
+    title: "滚滚长江东逝水",
+    desc: "滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水",
+    img: "https://picsum.photos/122/122",
+  },
+  {
+    id: 4,
+    title: "滚滚长江东逝水",
+    desc: "滚滚长江东逝水滚滚长江东逝水滚滚长江东逝水",
+    img: "https://picsum.photos/122/122",
+  },
+];
+</script>
+
+<template>
+  <section class="recommend">
+    <div class="head">
+      <img src="~assets/images/vector.png" alt="" />
+      <h1 class="title">游玩推荐</h1>
+      <img src="~assets/images/vector.png" style="transform: rotateY(180deg)" />
+    </div>
+    <div class="content">
+      <div class="list">
+        <div v-for="item in data" :key="item.id" class="list-item">
+          <img :src="item.img" />
+          <div class="list-item-content">
+            <div class="list-item-title">{{ item.title }}</div>
+            <div class="list-item-desc">{{ item.desc }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+.recommend {
+  margin-top: 30px;
+  margin-bottom: 50px;
+  .head {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    .title {
+      font-size: 70px;
+    }
+  }
+  .content {
+    margin-top: 20px;
+
+    .list {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      gap: 20px;
+      margin: 0 auto;
+      .list-item {
+        box-sizing: border-box;
+        display: flex;
+        width: 513px;
+        height: 198px;
+        padding: 38px 33px;
+        border: 1px solid rgb(255, 201, 194);
+        border-radius: 8px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        img {
+          width: 122px;
+          height: 122px;
+          border-radius: 50%;
+          margin-right: 40px;
+        }
+        .list-item-title {
+          margin-bottom: 14px;
+          font-size: 28px;
+        }
+        .list-item-desc {
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          letter-spacing: 0.2em;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,70 @@
+<script lang="ts" setup></script>
+
 <template>
-  <div>
-    <h1>山哈通</h1>
-    <button>12</button>
+  <div class="container">
+    <header>
+      <nav>
+        <div class="left">
+          <div class="left-item">山哈通</div>
+        </div>
+        <div class="right">
+          <div class="right-item">文化科普</div>
+          <div class="right-item">旅游推荐</div>
+          <div class="right-item mall">畲族商城</div>
+          <div class="right-item">登录/注册</div>
+        </div>
+      </nav>
+    </header>
+    <section class="banner">
+      <video autoplay loop muted class="banner-video">
+        <source src="~/assets/videos/banner.mp4" type="video/mp4" />
+      </video>
+    </section>
+    <IndexRecommend />
+    <IndexCulture />
   </div>
 </template>
-<style></style>
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 2;
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 1080px;
+    height: 64px;
+    margin: 0 auto;
+    font-size: 32px;
+    color: #fff;
+    .left {
+      display: flex;
+    }
+    .right {
+      display: flex;
+      align-items: center;
+      .right-item:not(:last-child) {
+        margin-right: 30px;
+      }
+      .mall {
+        padding: 5px 20px;
+        border: 2px solid rgb(255, 255, 255);
+        border-radius: 4px;
+      }
+    }
+  }
+}
+
+.banner {
+  height: 100vh;
+  width: 100%;
+  .banner-video {
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+  }
+}
+</style>
