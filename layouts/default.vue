c7d70b81da537190ff02c611aebead75bbc10916
yeyunwen <1571374338@qq.com>
2024-04-01 17:37:37 +0800

    feat: 添加默认layout

---
+++ layouts/default.vue
@@ -0,0 +1,121 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const indexStyle = computed(() => {
+  return {
+    index: route.name === "index",
+  };
+});
+
+const { user, isLogin, logout } = useUserInfo();
+
+const logoutHandler = () => {
+  logout();
+};
+
+const mallUrl = ref("http://localhost:3301");
+const { auth } = useAuth();
+
+const goToMall = () => {
+  if (auth.value.token) {
+    mallUrl.value = `${mallUrl.value}?token=${auth.value.token}`;
+  }
+  window.open(mallUrl.value, "_blank");
+};
+</script>
+
+<template>
+  <div class="default-layout" :class="indexStyle">
+    <slot name="header">
+      <header class="header">
+        <nav>
+          <div class="left">
+            <a class="left-item logo" href="/">山哈通</a>
+          </div>
+          <div class="right">
+            <div class="right-item">文化科普</div>
+            <a class="right-item" href="/travel">旅游推荐</a>
+            <a class="right-item mall" @click="goToMall"> 畲族商城 </a>
+            <div v-if="isLogin" class="right-item">
+              <a>{{ user.username }}</a>
+              <span style="margin: 0 8px">|</span>
+              <span class="right-item" @click="logoutHandler">退出</span>
+            </div>
+            <a v-else class="right-item" href="/login">登录/注册</a>
+          </div>
+        </nav>
+      </header>
+    </slot>
+    <main class="main">
+      <slot name="default"></slot>
+    </main>
+    <footer class="footer">
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "~/assets/styles/mixins.scss" as *;
+@use "~/assets/styles/var.scss" as *;
+
+.default-layout {
+  header {
+    @include useShtFont;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 2;
+    background-color: #fff;
+    nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 1080px;
+      height: 64px;
+      margin: 0 auto;
+      font-size: 32px;
+      color: #333;
+      .logo {
+        color: $sht-color-primary;
+      }
+      .left {
+        display: flex;
+      }
+      .right {
+        display: flex;
+        align-items: center;
+        .right-item:not(:last-child) {
+          margin-right: 30px;
+        }
+        .mall {
+          padding: 5px 20px;
+          border: 2px solid rgb(255, 255, 255);
+          border-radius: 4px;
+        }
+      }
+    }
+
+    &.index {
+      background-color: transparent;
+    }
+  }
+
+  main {
+    margin-top: 64px;
+  }
+
+  &.index {
+    header {
+      background-color: transparent;
+      nav {
+        color: #fff;
+      }
+    }
+    main {
+      margin-top: 0;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,7 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="container">
-    <header>
-      <nav>
-        <div class="left">
-          <div class="left-item">山哈通</div>
-        </div>
-        <div class="right">
-          <div class="right-item">文化科普</div>
-          <a class="right-item" href="/travel">旅游推荐</a>
-          <div class="right-item mall">畲族商城</div>
-          <a class="right-item" href="/login">登录/注册</a>
-        </div>
-      </nav>
-    </header>
+  <div class="index-container">
     <section class="banner">
       <video autoplay loop muted class="banner-video">
         <source src="~/assets/videos/banner.mp4" type="video/mp4" />
@@ -31,40 +18,6 @@ useSeoMeta({
 <style lang="scss" scoped>
 @use "~/assets/styles/mixins.scss" as *;
 
-header {
-  @include useShtFont;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 2;
-  nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 1080px;
-    height: 64px;
-    margin: 0 auto;
-    font-size: 32px;
-    color: #fff;
-    .left {
-      display: flex;
-    }
-    .right {
-      display: flex;
-      align-items: center;
-      .right-item:not(:last-child) {
-        margin-right: 30px;
-      }
-      .mall {
-        padding: 5px 20px;
-        border: 2px solid rgb(255, 255, 255);
-        border-radius: 4px;
-      }
-    }
-  }
-}
-
 .banner {
   height: 100vh;
   width: 100%;
@@ -72,6 +25,14 @@ header {
     width: 100%;
     height: 100%;
     object-fit: fill;
+  }
+}
+</style>
+
+<style lang="scss">
+.index-container {
+  header {
+    background: transparent !important;
   }
 }
 </style>
